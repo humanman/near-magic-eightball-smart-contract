6caa5f25937a71649122adb41ddce5037bf6508d
Ben Sayeg <bensayeg@gmail.com>
2021-04-22 19:57:19 -0400

    reverted back to functions. Calls working now. Method for adding a new answer costs too much gas to run :/

--- assembly/index.ts
+++ assembly/index.ts
@@ -10,27 +10,21 @@ import { MAXLEN, questions } from './model';
  * - it has the side effect of appending to the log
  */
 
-@nearBindgen
-export class Contract {
-  constructor() {
-    this._init()
-  }
-  
-  answers: PersistentVector<string> = new PersistentVector<string>('av');
+  const answers: PersistentVector<string> = new PersistentVector<string>('av');
   // ------------------------------------------------------------------
   // CHANGE methods
   // ------------------------------------------------------------------
-  answerMyQuestion(question: string): string {
+  export function answerMyQuestion(question: string): string {
     logging.log("answerMyQuestion() was called");
     assert(question.length > 0, "Question can not be blank.");
     // TODO: implement RegEx when AS does :/
     // check for What, When, Where, Why at beginning.
     // const regexp: RegExp = /^what|when|where|why/i;
     // assert(regexp.test(question) == false, "Please ask Yes or No questions only.");
-    const rng = new RNG<u8>(1, this.answers.length);
+    const rng = new RNG<u8>(1, answers.length);
     const rollIdx = rng.next();
     logging.log(rollIdx);
-    return this.answers[rollIdx];
+    return answers[rollIdx];
   }
 
   /**
@@ -43,7 +37,7 @@ export class Contract {
    * - it has the side effect of appending to the log
    */
   //  TODO: ideally this saves a record of the transaction (question / answer), but that may already be on the chain rendering this method redundant
-  saveMyQuestion(question: string): boolean {
+  export function saveMyQuestion(question: string): boolean {
     logging.log("saveMyQuestion() was called");
 
     // assert(message.length > 0, "Message can not be blank.");
@@ -53,7 +47,8 @@ export class Contract {
     return true;
   }
 
-  addNewAnswerToMagic8Ball(answerToAdd: string): void {
+  export function addNewAnswerToMagic8Ball(answerToAdd: string): void {
+    logging.log(answerToAdd);
     // check length
     assert(answerToAdd.length > 0 && answerToAdd.length <= MAXLEN, `Submission must be more than 0 and fewer than ${MAXLEN.toString()} characters long.`)
 
@@ -61,38 +56,38 @@ export class Contract {
     // should be new answer
     const formattedAnswerToAdd = answerToAdd.substring(0, 1).toUpperCase() + answerToAdd.substring(1).toLowerCase();
     logging.log(formattedAnswerToAdd);
-    assert(this._vectorHasContents(this.answers, formattedAnswerToAdd) == false, "That answer already exists!")
-    this.answers.push(formattedAnswerToAdd);
+    assert(this._vectorHasContents(answers, formattedAnswerToAdd) == false, "That answer already exists!")
+    answers.push(formattedAnswerToAdd);
   }
 
   // ------------------------------------------------------------------
   // private methods
   // ------------------------------------------------------------------
-  private _init(): void {
-    // this.answers = new PersistentVector<string>('av');
-    this.answers.push('As I see it, yes');
-    this.answers.push('Ask again later.');
-    this.answers.push('Better not tell you now.');
-    this.answers.push('Cannot predict now.');
-    this.answers.push('Concentrate and ask again.');
-    this.answers.push('Don\'t count on it.');
-    this.answers.push('It is certain.');
-    this.answers.push('It is decidedly so.');
-    this.answers.push('Most likely.');
-    this.answers.push('My reply is no.');
-    this.answers.push('My sources say no.');
-    this.answers.push('Outlook not so good.');
-    this.answers.push('Outlook good.');
-    this.answers.push('Reply hazy, try again.');
-    this.answers.push('Signs point to yes.');
-    this.answers.push('Very doubtful.');
-    this.answers.push('Without a doubt.');
-    this.answers.push('Yes.');
-    this.answers.push('Yes - definitely.');
-    this.answers.push('Yes may rely on it.');
+  function _init(): void {
+    // answers = new PersistentVector<string>('av');
+    answers.push('As I see it, yes');
+    answers.push('Ask again later.');
+    answers.push('Better not tell you now.');
+    answers.push('Cannot predict now.');
+    answers.push('Concentrate and ask again.');
+    answers.push('Don\'t count on it.');
+    answers.push('It is certain.');
+    answers.push('It is decidedly so.');
+    answers.push('Most likely.');
+    answers.push('My reply is no.');
+    answers.push('My sources say no.');
+    answers.push('Outlook not so good.');
+    answers.push('Outlook good.');
+    answers.push('Reply hazy, try again.');
+    answers.push('Signs point to yes.');
+    answers.push('Very doubtful.');
+    answers.push('Without a doubt.');
+    answers.push('Yes.');
+    answers.push('Yes - definitely.');
+    answers.push('Yes may rely on it.');
   }
    
-  private _vectorHasContents(
+  function _vectorHasContents(
     vector: PersistentVector<string>,
     target: string
   ): bool {
@@ -104,5 +99,5 @@ export class Contract {
     }
     return false;
   }
-}
+
 
---
+++ execute.js
@@ -0,0 +1,58 @@
+// Load environment variables
+require("dotenv").config();
+
+// Load NEAR Javascript API components
+const near = require("near-api-js");
+
+// Directory where NEAR credentials are going to be stored
+const credentialsPath = "./credentials";
+
+// Configure the keyStore to be used with the NEAR Javascript API
+const UnencryptedFileSystemKeyStore = near.keyStores.UnencryptedFileSystemKeyStore;
+const keyStore = new UnencryptedFileSystemKeyStore(credentialsPath)
+console.log('key store', keyStore)
+// Setup default client options
+const options = {
+  networkId: process.env.NEAR_NETWORK,
+  nodeUrl: process.env.NEAR_NODE_URL,
+  walletUrl: `https://wallet.${process.env.NEAR_NETWORK}.near.org`,
+  helperUrl: `https://helper.${process.env.NEAR_NETWORK}.near.org`,
+  explorerUrl: `https://explorer.${process.env.NEAR_NETWORK}.near.org`,
+  accountId: process.env.NEAR_ACCOUNT,
+  deps: {
+    keyStore: keyStore
+  }
+}
+
+async function main() {
+  // Configure the client with options and our local key store
+  const client = await near.connect(options);
+  const account = await client.account(options.accountId);
+
+  // // We'are using the same contract name, feel free to create a different one.
+  const contractName = options.accountId;
+
+  // // Construct a new contract object, we'll be using it to perform calls
+  const contract = new near.Contract(account, contractName, {
+    // viewMethods: [""],   // TODO: add methods to list answers + previously asked questions
+    changeMethods: ["answerMyQuestion","addNewAnswerToMagic8Ball"], // our write function
+    sender: options.accountId,   // account used to sign contract call transactions
+  });
+ 
+
+  // // We will send the current date when calling `answerMyQuestion`
+  const value = "Will I be a NEARionnaire?";
+
+  console.log(`Calling contract call 'answerMyQuestion' with '${value}'`);
+  const result = await contract.answerMyQuestion({ question: value });
+
+  // // Log magic8Ball's response
+  console.log("ResultAsked:", result);
+  // const newAnswer = "Hmm... I'm not sure about that."
+  // const newItem = await contract.addNewAnswerToMagic8Ball({ answerToAdd: "I am not sure about that." });
+  // console.log("ResultAdded:", newItem);
+};
+
+main();
+
+// $watch -d -n 1 yarn storage $CONTRACT
